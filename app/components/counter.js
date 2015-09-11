70c88779f66b489303e75b5b2f652b637d9c35af
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-09-11 09:07:23 -0400

    apply some style to example

--- app/components/counter.js
+++ app/components/counter.js
@@ -1,9 +1,22 @@
 import React, {
+  StyleSheet,
   Component,
   View,
   Text,
-  TouchableHighlight
+  TouchableOpacity
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    width: 100,
+    height: 30,
+    padding: 10,
+    backgroundColor: 'lightgray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 3
+  }
+});
 
 export default class Counter extends Component {
   constructor(props) {
@@ -14,14 +27,14 @@ export default class Counter extends Component {
     const { counter, increment, decrement } = this.props;
 
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>{counter}</Text>
-        <TouchableHighlight onPress={increment}>
+        <TouchableOpacity onPress={increment} style={styles.button}>
           <Text>up</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={decrement}>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={decrement} style={styles.button}>
           <Text>down</Text>
-        </TouchableHighlight>
+        </TouchableOpacity>
       </View>
     );
   }
