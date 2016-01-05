1b5970e0cc2fc6a186e7f895b01e5382934faea7
Avindra Goolcharan <aavindraa@gmail.com>
2016-01-04 19:50:42 -0500

    combine destructuring into import statement

--- Counter/index.ios.js
+++ Counter/index.ios.js
@@ -1,10 +1,6 @@
 'use strict';
 
-import React from 'react-native';
+import React, { AppRegistry } from 'react-native';
 import App from './app/containers/app';
-
-var {
-  AppRegistry
-} = React;
 
 AppRegistry.registerComponent('Counter', () => App);
